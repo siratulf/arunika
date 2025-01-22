9d462946c00e6b61639925b9232c3548cdd549fa
siratulf <siratulfirdaus30@gmail.com>
2025-01-22 20:33:36 +0700

    Update Beranda.py

--- arunika_app/Beranda.py
+++ arunika_app/Beranda.py
@@ -8,15 +8,13 @@ st.set_page_config(
 
 # Add custom CSS to hide the GitHub icon
 st.markdown(
-    """
     <style>
     .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
     .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
     .viewerBadge_text__1JaDK {
         display: none;
     }
-    </style>
-    """,
+    </style>,
     unsafe_allow_html=True
 )
 
