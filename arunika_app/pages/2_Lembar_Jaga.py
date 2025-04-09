ef5cb0979a913f223f4ca1d2d3788d9cd3dd2973
siratulf <siratulfirdaus30@gmail.com>
2025-04-09 21:46:43 +0700

    Update 2_Lembar_Jaga.py

--- arunika_app/pages/2_Lembar_Jaga.py
+++ arunika_app/pages/2_Lembar_Jaga.py
@@ -174,7 +174,7 @@ with tab1:
             else:
                 data = pd.concat([data, pd.DataFrame([form_values])], ignore_index=True)
                 data = data.drop_duplicates(subset= ["Nama PML", "Nama PPL", "NKS", "Nomor Urut Sampel"], keep= "last")
-                data.to_csv("arunika_app/updated_response.csv", index=False)
+                data.to_csv(csv_file_path, index=False)
                 st.success("Jawabanmu berhasil dikirim, kamu bisa lakukan review pada menu Review Lembar Jaga ya!")
 
     #Opsi CSV Upload
